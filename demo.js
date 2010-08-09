0f3401bcfb523af98026ff04843a2112026a06c6
Aurynn Shaw <aurynn@eris.local>
2010-08-09 11:05:09 -0700

    Updated postgres-pure.js to compile and begin to actually work. Modification to demo.js to provide correct syntax ($1).
    Currently having an issue with the DESCRIBE message; modified buffer_extras to try to solve it. No luck as yet.

--- demo.js
+++ demo.js
@@ -1,9 +1,12 @@
 var sys = require("sys");
-var Postgres = require("./postgres");
+var pg = require("./lib/postgres-pure");
+pg.DEBUG=true;
 
-var db = new Postgres.Connection("dbname", "username", "password");
-db.query("SELECT * FROM test");
-db.query("SELECT * FROM test", function (data) {
-  sys.p(data);
+var db = new pg.connect("pgsql://aurynn:12345@localhost:5432/akhyana");
+db.prepare("SELECT * FROM users where id = $1;", function (sth) {
+    sth.execute([1], function (rs) {
+        for (var i = 0; i <= rs.length; i++) {
+            sys.puts(rs[i]);
+        }
+    });
 });
-db.close();
