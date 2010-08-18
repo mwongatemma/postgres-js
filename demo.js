3724aae1cdaa4263a0094f8d18b05d8ef7acbdfa
Aurynn Shaw <aurynn@eris.local>
2010-08-17 21:02:08 -0700

    Added the "Bind" message. Modification to the Prepare blocks so they actually work. Prepare now yields more or less correctly to the Execute message(s)
    Bind message isn't working, and the code needs a lot of optimization and thought before it can be considered production-usable

--- demo.js
+++ demo.js
@@ -3,7 +3,8 @@ var pg = require("./lib/postgres-pure");
 pg.DEBUG=true;
 
 var db = new pg.connect("pgsql://aurynn:12345@localhost:5432/akhyana");
-db.prepare("SELECT * FROM users where id = $1;", function (sth) {
+db.prepare("SELECT * FROM users WHERE id = $1;", function (sth) {
+    sys.debug("in callback");
     sth.execute([1], function (rs) {
         for (var i = 0; i <= rs.length; i++) {
             sys.puts(rs[i]);
