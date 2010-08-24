ff835890bf09022ed3136dcce438230195504c02
Aurynn Shaw <aurynn@eris.local>
2010-08-24 08:15:33 -0700

    Prepared queries now work mostly as expected. :)

--- demo.js
+++ demo.js
@@ -1,13 +1,19 @@
 var sys = require("sys");
 var pg = require("./lib/postgres-pure");
-pg.DEBUG=true;
+pg.DEBUG=0;
 
 var db = new pg.connect("pgsql://aurynn:12345@localhost:5432/akhyana");
 db.prepare("SELECT * FROM users WHERE id = $1;", function (sth) {
     sys.debug("in callback");
     sth.execute([1], function (rs) {
-        for (var i = 0; i <= rs.length; i++) {
-            sys.puts(rs[i]);
+        sys.debug("In execute");
+        for (var i = 0; i < rs.length; i++) {
+            for (var key in rs[i]) {
+                if (rs[i].hasOwnProperty(key)) {
+                    sys.puts(key +": " +rs[i][key]);
+                }
+            }
         }
+        db.end();
     });
 });
