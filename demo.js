6922cf8ad46e4d2b3659ea52822f686496007000
Aurynn Shaw <aurynn@eris.local>
2010-08-25 16:14:17 -0700

    Adding in support for ? -> $1, etc. syntax. Very simple regex thus far, and will likely prove to be brittle.

--- demo.js
+++ demo.js
@@ -1,12 +1,10 @@
 var sys = require("sys");
 var pg = require("./lib/postgres-pure");
-pg.DEBUG=0;
+pg.DEBUG=1;
 
-var db = new pg.connect("pgsql://aurynn:12345@localhost:5432/akhyana");
-db.prepare("SELECT * FROM users WHERE id = $1;", function (sth) {
-    sys.debug("in callback");
+var db = new pg.connect("pgsql://test:12345@localhost:5432/template1");
+db.prepare("SELECT ?::int", function (sth) {
     sth.execute([1], function (rs) {
-        sys.debug("In execute");
         for (var i = 0; i < rs.length; i++) {
             for (var key in rs[i]) {
                 if (rs[i].hasOwnProperty(key)) {
