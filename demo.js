cc86ee65a975681edcde57e79ea02c21dcbe6f47
Aurynn Shaw <aurynn@eris.local>
2010-09-27 20:28:19 -0700

    Re-broke prepared queries. Mostly successfully migrated to the internal Transaction object metaphor. Queries are once again working successfully in the TX format.

--- demo.js
+++ demo.js
@@ -3,18 +3,29 @@ var pg = require("./lib/postgres-pure");
 pg.DEBUG=0;
 
 var db = new pg.connect("pgsql://test:12345@localhost:5432/template1");
-db.prepare("SELECT ?::int", function (sth) {
-    sth.execute([1], function (rs) {
-        for (var i = 0; i < rs.length; i++) {
-            for (var key in rs[i]) {
-                if (rs[i].hasOwnProperty(key)) {
-                    sys.puts(key +": " +rs[i][key]);
-                }
-            }
-        }
-        db.end();
-    });
-    sth.execute([2], function (rs) {
-        sys.puts(sys.inspect(rs));
-    })
+db.query("SELECT 1::int as foobar;", function (rs) {
+    sys.puts(sys.inspect(rs));
+    db.close();
 });
+
+// db.prepare("SELECT ?::int AS foobar", function (sth) {
+//     sth.execute([1], function (rs) {
+//         sys.puts(sys.inspect(rs));
+//     });
+//     sth.execute([2], function (rs) {
+//         sys.puts(sys.inspect(rs));
+//         
+//     });
+// });
+// 
+// db.transaction(function (tx) {
+//     tx.query("SELECT ?::int AS foobar", 1, function (rs) {
+//         sys.puts(sys.inspect(rs));
+//     });
+//     tx.prepare("SELECT ?::int AS foobar", function (sth) {
+//         sth.execute(2, function (rs) {
+//             sys.puts(sys.inspect(rs));
+//         });
+//     });
+//     tx.commit();
+// });
