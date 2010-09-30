54b003535e2dc917dbe5931d31f64f3ed12de6d3
Aurynn Shaw <aurynn@eris.local>
2010-09-30 08:03:45 -0700

    Multiple queries & transactioned queries now working correctly. Prepared queries are still broken.
    Underlying pipeline/query buffer mechanism appears to be working correctly.

--- demo.js
+++ demo.js
@@ -1,22 +1,28 @@
 var sys = require("sys");
 var pg = require("./lib/postgres-pure");
-pg.DEBUG=0;
+pg.DEBUG=1;
 
 var db = new pg.connect("pgsql://test:12345@localhost:5432/template1");
-db.query("SELECT 1::int as foobar;", function (rs) {
+db.query("SELECT 1::int as foobar;", function (rs, tx) {
     sys.puts(sys.inspect(rs));
-    db.close();
+    tx.query("SELECT 2::int as foobartwo", function (rs) {
+        sys.puts(sys.inspect(rs));
+    });
+    // db.close();
 });
 
-// db.prepare("SELECT ?::int AS foobar", function (sth) {
-//     sth.execute([1], function (rs) {
+// db.prepare("SELECT ?::int AS foobar", function (sth, tx) {
+//     sth.execute(1, function (rs) {
 //         sys.puts(sys.inspect(rs));
 //     });
-//     sth.execute([2], function (rs) {
+//     sth.execute(2, function (rs) {
 //         sys.puts(sys.inspect(rs));
 //         
 //     });
+//     // db.close();
 // });
+db.close();
+
 // 
 // db.transaction(function (tx) {
 //     tx.query("SELECT ?::int AS foobar", 1, function (rs) {
