f56cee15867bd74348d700e12c9df76972bf97c7
Aurynn Shaw <aurynn@ananke.local>
2010-10-09 21:16:05 -0700

    Prepared queries work As Expected. Lots of debugging symbols still defaulting to on, that need to be cleaned up.
    Other code that needs to be written:
     * Disengagement code, so that a messages block that's retired can be primed when it is placed back onto the query buffer. (say a .execute gets run later.)
     * LISTEN. It makes so much sense.

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var sys = require("sys");
 var pg = require("./lib/postgres-pure");
-pg.DEBUG=1;
+// pg.DEBUG=1;
 
 var db = new pg.connect("pgsql://test:12345@localhost:5432/template1");
 db.query("SELECT 1::int as foobar;", function (rs, tx) {
@@ -11,16 +11,16 @@ db.query("SELECT 1::int as foobar;", function (rs, tx) {
     // db.close();
 });
 
-// db.prepare("SELECT ?::int AS foobar", function (sth, tx) {
-//     sth.execute(1, function (rs) {
-//         sys.puts(sys.inspect(rs));
-//     });
-//     sth.execute(2, function (rs) {
-//         sys.puts(sys.inspect(rs));
-//         
-//     });
-//     // db.close();
-// });
+db.prepare("SELECT ?::int AS foobar", function (sth, tx) {
+    sth.execute(1, function (rs, tx) {
+        sys.puts(sys.inspect(rs));
+    });
+    sth.execute(2, function (rs) {
+        sys.puts(sys.inspect(rs));
+        
+    });
+    // db.close();
+});
 db.close();
 
 // 
