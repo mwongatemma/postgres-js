13041fb94cffea55a4e948d86365d2a86ae96d1b
Aurynn Shaw <aurynn@ananke.local>
2010-10-27 23:00:55 -0700

    Adding tentative support for the transactional objects.

--- demo.js
+++ demo.js
@@ -2,24 +2,24 @@ var sys = require("sys");
 var pg = require("./lib/postgres-pure");
 // pg.DEBUG=4;
 
-var db = new pg.connect("pgsql://test:12345@localhost:5432/returning_test");
-db.query("SELECT 1::querytest;", function (error, rs, tx) {
-    if (error) {
-        console.log("Error!");
-        console.log(error);
-        console.log(error.code);
-    }
-    else {
-        console.log(sys.inspect(rs));
-    }
-    
-    tx.query("SELECT 2::int as querytest2", function (err, rs) {
-        console.log(sys.inspect(rs));
-    });
-    tx.query("SELECT 3::qt" ,function (err, rs) {
-        console.log(sys.inspect(err));
-    })
-});
+var db = new pg.connect("pgsql://test:12345@localhost:5432/insert_test");
+// db.query("SELECT 1::querytest;", function (error, rs, tx) {
+//     if (error) {
+//         console.log("Error!");
+//         console.log(error);
+//         console.log(error.code);
+//     }
+//     else {
+//         console.log(sys.inspect(rs));
+//     }
+//     
+//     tx.query("SELECT 2::int as querytest2", function (err, rs) {
+//         console.log(sys.inspect(rs));
+//     });
+//     tx.query("SELECT 3::qt" ,function (err, rs) {
+//         console.log(sys.inspect(err));
+//     })
+// });
 
 // db.prepare("INSERT INTO returning_test (val) VALUES (?) RETURNING id, val", function (sth) {
 //     sth.execute("text value", function(e, rs) {
@@ -32,35 +32,47 @@ db.query("SELECT 1::querytest;", function (error, rs, tx) {
 //     });
 // });
 
-// db.prepare().on("some_event"); 
-
 // db.prepare("SELECT ?::int AS preparetest", function (sth, tx) {
 //     sth.execute(1, function (err, rs) {
-//         sys.puts(sys.inspect(rs));
+//         console.log(sys.inspect(rs));
 //     });
 //     sth.execute(2, function (err, rs) {
-//         sys.puts(sys.inspect(rs));
+//         console.log(sys.inspect(rs));
 //         
 //     });
 //     tx.prepare("SELECT ?::int AS preparetest2", function (sth) {
 //        sth.execute(3, function (err, rs) {
-//            sys.puts(sys.inspect(rs));
+//            console.log(sys.inspect(rs));
+//        }) ;
+//     });
+//     tx.prepare("SELECT ?::int AS preparetest2", function (sth) {
+//        sth.execute(4, function (err, rs) {
+//            console.log(sys.inspect(rs));
 //        }) ;
 //     });
 // });
 
-// db.transaction(function (tx) {
-//     // tx.begin();
-//     tx.query("SELECT ?::int AS txtest1", 1, function (rs) {
-//         sys.puts(sys.inspect(rs));
-//     });
-//     tx.prepare("SELECT ?::int AS txtest2", function (sth) {
-//         sth.execute(2, function (rs) {
-//             sys.puts(sys.inspect(rs));
-//         });
-//     });
-//     // tx.commit();
-// });
+db.transaction(function (tx) {
+    tx.begin();
+    // tx.query("INSERT INTO insert_test (val) VALUES (?) RETURNING id", "test value", function (err,rs) {
+    //     console.log(sys.inspect(rs));
+    // });
+    // tx.prepare("INSERT INTO insert_test (val) VALUES (?) RETURNING id", function (sth) {
+    //     sth.execute("twooooo", function (err, rs) {
+    //         console.log(sys.inspect(rs));
+    //     });
+    // });
+    tx.query("SELECT 1::barrrrrr", function (err, rs) {
+        console.log(sys.inspect(err));
+    });
+    tx.query("SELECT 1::int as foobar", function (err, rs) {
+        console.log(sys.inspect(err));
+    });
+    tx.rollback();
+    tx.query("SELECT 1::int as foobarbaz", function (err, rs) {
+        console.log(sys.inspect(rs));
+    });
+});
 db.close();
 
 // db.prepare(query, function (sth, tx) {
